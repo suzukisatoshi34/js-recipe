4e664bca7143714bcdcf80ec2d4ce0301fdf57b2
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-04-16 09:03:19 +0900

    week1 予習

--- 1_workspace/1/3の倍数/byousin1.js
+++ 1_workspace/1/3の倍数/byousin1.js
@@ -1,24 +1,24 @@
-const genkiFunction = function (number) {
+const genkiFunction = function(number) {
   // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
-  n = 0;
-  const countup = function () {
-    n += 1;
+  n = 0
+  const countup = function() {
+    n += 1
     if (
       n % 3 === 0 &&
       n % 10 != 3 &&
       n - (n % 10) != 30 &&
       n - (n % 100) != 300
     ) {
-      console.log(n + "!!!!!!!");
+      console.log(n + "!!!!!!!")
     }
     if (n % 10 === 3 && n - (n % 10) != 30 && n - (n % 100) != 300) {
-      console.log(n + "!!!!!!!");
+      console.log(n + "!!!!!!!")
     }
     if (n - (n % 10) === 30 && n - (n % 100) != 300) {
-      console.log(n + "!!!!!!!");
+      console.log(n + "!!!!!!!")
     }
     if (n - (n % 100) === 300) {
-      console.log(n + "!!!!!!!");
+      console.log(n + "!!!!!!!")
     }
     if (
       n % 3 != 0 &&
@@ -26,12 +26,12 @@ const genkiFunction = function (number) {
       n - (n % 10) != 30 &&
       n - (n % 100) != 300
     ) {
-      console.log(n);
+      console.log(n)
     }
     if (n >= number) {
-      clearInterval(id);
-      console.log("終わり！！");
+      clearInterval(id)
+      console.log("終わり!!")
     }
-  };
-  id = setInterval(countup, 1000);
-};
+  }
+  id = setInterval(countup, 1000)
+}
