62baefdb41ecfd95f1ac482699bb0f4cfc7f5769
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-04-16 09:22:22 +0900

    確認

--- 1_workspace/1/□と○/event.js
+++ 1_workspace/1/□と○/event.js
@@ -1,7 +1,6 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function() {
-  // figure に rounded クラスを追加する
   figure.classList.toggle("rounded")
 }
 figure.onmouseover = function() {
