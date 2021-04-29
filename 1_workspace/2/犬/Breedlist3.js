9e27524524faebe07b3808a9fab5329834fc7705
suzukisatoshi34 <satoshi.basuke.34@icloud.com>
2021-04-29 18:16:03 +0900

    犬枚数指定できた

--- 1_workspace/2/犬/Breedlist3.js
+++ 1_workspace/2/犬/Breedlist3.js
@@ -1,15 +1,16 @@
-const imageElementoll = document.getElementById("dog-image1")
+const imageElements = document.getElementById("container")
 const button = document.getElementById("button")
 const kind = document.getElementById("dog-select")
 const maisu = document.getElementById("input-todo2")
 
-button.onclick = function() {
+const hyouji = function() {
   console.log(kind.value)
   console.log(maisu.value)
   const N = maisu.value
   const code = "https://dog.ceo/api/breed/" + kind.value + "/images/random"
   for (let n = 0; n <= N; n++) {
     const imageElement = document.createElement("img")
+    imageElement.src = ""
     fetch(code)
       .then((res) => {
         return res.json() // 結果を json として読み込む
@@ -17,41 +18,19 @@ button.onclick = function() {
       .then((data) => {
         imageElement.src = data.message // 画像を表示する
       })
-    imageElementoll.appendChild(imageElement)
+
+    imageElements.appendChild(imageElement)
   }
+}
+button.onclick = function() {
+  hyouji()
 }
 
 maisu.addEventListener("keypress", test_ivent)
 
 function test_ivent(e) {
   if (e.keyCode === 13) {
-    console.log(kind.value)
-    console.log(maisu.value)
-    const N = maisu.value
-    const code = "https://dog.ceo/api/breed/" + kind.value + "/images/random"
-    for (let n = 0; n <= N; n++) {
-      fetch(code)
-        .then((res) => {
-          return res.json() // 結果を json として読み込む
-        })
-        .then((data) => {
-          if (n === 0) {
-            imageElement1.src = data.message // 画像を表示する
-          }
-          if (n === 1) {
-            imageElement2.src = data.message
-          }
-          if (n === 2) {
-            imageElement3.src = data.message
-          }
-          if (n === 3) {
-            imageElement4.src = data.message
-          }
-          if (n === 4) {
-            imageElement5.src = data.message
-          }
-        })
-    }
+    hyouji()
   }
 }
 
@@ -59,32 +38,6 @@ kind.addEventListener("keypress", test_ivent)
 
 function test_ivent(e) {
   if (e.keyCode === 13) {
-    console.log(kind.value)
-    console.log(maisu.value)
-    const N = maisu.value
-    const code = "https://dog.ceo/api/breed/" + kind.value + "/images/random"
-    for (let n = 0; n <= N; n++) {
-      fetch(code)
-        .then((res) => {
-          return res.json() // 結果を json として読み込む
-        })
-        .then((data) => {
-          if (n === 0) {
-            imageElement1.src = data.message // 画像を表示する
-          }
-          if (n === 1) {
-            imageElement2.src = data.message
-          }
-          if (n === 2) {
-            imageElement3.src = data.message
-          }
-          if (n === 3) {
-            imageElement4.src = data.message
-          }
-          if (n === 4) {
-            imageElement5.src = data.message
-          }
-        })
-    }
+    hyouji()
   }
 }
